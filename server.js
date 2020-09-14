545562a3822103647535470239d38fa495026754
Emrullah13 <61323677+Emrullah13@users.noreply.github.com>
2020-09-14 20:29:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -75,15 +75,5 @@ function bindEvents(bot) {
 
     bindEvents(bot);
   }
-}
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-  
+}  
 });
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`https://alternoss.herokuapp.com/`);
-}, 150);
