57c250f618b336c3ab8dca7599155faa9c2bd32f
Emrullah13 <61323677+Emrullah13@users.noreply.github.com>
2021-02-15 12:50:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -76,14 +76,14 @@ function bindEvents(bot) {
     bindEvents(bot);
   }
 }
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-  
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 150000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Alternos bot aktif')
+})
+
+app.listen(port, () => {
+  console.log(`${port} portuyla aktif.`)
+})
